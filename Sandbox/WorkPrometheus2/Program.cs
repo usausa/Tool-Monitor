d591471bc337e33e3092520a1b312b5e8396d5e8
usausa <machi.pon@gmail.com>
2021-12-28 17:50:27 +0900

    Update

--- Sandbox/WorkPrometheus2/Program.cs
+++ Sandbox/WorkPrometheus2/Program.cs
@@ -1,9 +1,40 @@
+using System.Diagnostics.Metrics;
+
+using OpenTelemetry.Metrics;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenTelemetryMetrics(configure =>
+{
+    configure.AddAspNetCoreInstrumentation();
+    configure.AddMeter("ApplicationMetrics");
+    configure.AddPrometheusExporter();
+    configure.AddConsoleExporter(options =>
+    {
+        options.MetricReaderType = MetricReaderType.Periodic;
+        options.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 10000;
+    });
+});
 
 // Add services to the container.
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello");
+app.MapGet("/", () =>
+{
+    ApplicationMetrics.IncrementHello();
+    return "Hello";
+});
+
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.Run();
+
+public static class ApplicationMetrics
+{
+    private static readonly Meter Meter = new("ApplicationMetrics");
+
+    private static readonly Counter<int> HelloCounter = Meter.CreateCounter<int>("call_hello");
+
+    public static void IncrementHello() => HelloCounter.Add(1);
+}
